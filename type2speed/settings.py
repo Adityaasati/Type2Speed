dbe6a2221422d98ead145c92ded06bd2b3b9bc54
Aditya Asati <adityaasati01@gmail.com>
2025-02-23 14:23:54 +0530

    Added site/ and updated settings

--- type2speed/settings.py
+++ type2speed/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6qjf&&25x0#orvqj&bev2jw9nu_6-!k*saw2g6pzj&9732&j0u
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['www.type2speed.com','type2speed.com','167.71.227.79','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['167.71.227.79','127.0.0.1','localhost']
 
 
 
