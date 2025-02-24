a2d9e06f69f4394d633b97578c3c5f0ca45607e8
Aditya Asati <adityaasati01@gmail.com>
2025-02-24 17:55:16 +0530

    Favicons

--- type2speed/settings.py
+++ type2speed/settings.py
@@ -148,6 +148,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'typing_app', 'static'),  # Ensure this directory exists
 ]
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
